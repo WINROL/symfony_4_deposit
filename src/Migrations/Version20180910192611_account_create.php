1d35ec8c9f777aef5e80a938c6527c13bd1fbfe8
Ruslan Smyk <winrilz@gmail.com>
2018-09-10 23:01:42 +0300

    done migrations.

--- src/Migrations/Version20180910192611_account_create.php
+++ src/Migrations/Version20180910192611_account_create.php
@@ -17,7 +17,7 @@ final class Version20180910192611_account_create extends AbstractMigration
               id BIGINT AUTO_INCREMENT NOT NULL,
               client_id BIGINT NOT NULL,
               currency VARCHAR(3) NOT NULL,
-              sum DOUBLE(9,2),
+              sum DOUBLE(9,2) NOT NULL DEFAULT 0.00,
               PRIMARY KEY(id),
               CONSTRAINT FK_client_account
               FOREIGN KEY (client_id) REFERENCES client(id)
